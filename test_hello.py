421cb07ba05749085aa4990591be04a257d2d609
pt20 <72731467+pt20@users.noreply.github.com>
2020-10-13 14:38:11 +0200

    Create test_hello.py

---
+++ test_hello.py
@@ -0,0 +1,8 @@
+import pytest
+from hello import print_hello_world 
+
+def test_print_hello_world():
+    expected = "hello world!"
+    result = print_hello_world()
+    
+    assert expected == result
